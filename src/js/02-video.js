154e50f9b81ce648b39618b661bbc57d74832df3
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-07-21 21:39:25 +0300

    upd files task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = "currentTimeKey";
+const currentTime = localStorage.getItem(localStorageKey);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(localStorageKey, data.seconds);
+  }, 1000),
+  );
