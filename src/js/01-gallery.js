7e72bdd2eaff7caa2a899cf18d0d300fdaef0c5b
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-07-21 14:37:32 +0300

    upd f-s

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,49 +6,19 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
-console.log(gallery);
 
+const gallery = document.querySelector(".gallery");
 const items = galleryItems.map(({ preview, original, description }) => {
   return `
-  <div class="gallery__item">
-  <a class="gallery__link" href="#${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
-  `}).join('');
-  
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`
+}).join("");
+
 gallery.insertAdjacentHTML("afterbegin", items);
-  
-gallery.addEventListener('click', onImgClick);
 
-const instance = basicLightbox.create(`<img src="">`,
-  {
-    onShow: instance => {
-      window.addEventListener('keydown', keyDown);
-    },
-    onClose: instance => {
-      window.addEventListener('keydown', keyDown);
-    }
-  });
-
-function onImgClick(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-  instance.element().querySelector('img').src = event.target.dataset.source;
-  instance.show();
-}
-
-function keyDown(event) {
-  if (event.key === 'Escape') { 
-    instance.close();
-    return;
-  }
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
